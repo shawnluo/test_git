de22e2b8837032d9c11a4ff715a74de6de74c570
Goodaytodie <andersluo@hotmail.com>
2023-01-05 04:39:17 -0800

    update

--- test2.c
+++ test2.c
@@ -2,22 +2,72 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <assert.h>
 typedef struct node {
     int index;
-    int rep;
+    struct node *next;
 } Node, *pNode;
 
 #define SIZE sizeof(Node)
 
-pNode myData;
+int myStrcpy(char *src, char *des) {
+    if(!src || !des) {
+        printf("src or des is null!\n");
+        return -1;
+    }
+    //char *tmp_des = des;
 
+    while(*src != '\0') {
+        *des++ = *src++;
+    }
+    *des = '\0';
+
+    return 0;
+}
+
+//TODO 1: 
+int isSub(char *str, char *subStr) {
+    if(!str || !subStr) {
+        printf("empty str\n");
+        return -1;
+    }
+
+    char *p_str = NULL;
+    char *p_substr = NULL;
+    char *p_tmp = NULL;
+
+    for(p_str = str; *p_str; p_str++) {
+        p_tmp = p_str;
+        for(p_substr = subStr; *p_substr; p_substr++) {
+            if(*p_tmp++ != *p_substr) {
+                break;
+            }
+        }
+        if(!*p_substr) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+//TODO 2: 01
+int common_uniq_str() {
+    int pos = -1;
+}
 
 int main(void) {
-    char p[100] = {0};
-    char s[] = "good";
-    strcpy(p, s);
-    printf("%s\n", p);
+    char src[] = "good";
+    //char *des = (char *)malloc(100);
+    //memset(des, 0, 100);
+    char des[20] = {0};
+    if(myStrcpy(src, des) == -1) {
+        printf("wrong des or src!\n");
+    }
+    printf("%s\n", des);
+
+    int ret = isSub(src, "oqd");
+    printf("ret = %d\n", ret);
 
     return 0;
 }
