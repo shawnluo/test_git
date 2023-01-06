f4669649883b2bfd490593e7009c1d52fbf33930
Goodaytodie <andersluo@hotmail.com>
2023-01-06 04:17:21 -0800

    update

--- chapter2_array.c
+++ chapter2_array.c
@@ -180,6 +180,44 @@ void sort_square(int *arr, int size) {
 }
 
 
+//[11] remove duplicated elements
+//
+void rm_dup_elements(char *str) {
+    int hash[256] = {0};
+    int size = strlen(str);
+    int slow, fast;
+
+    for(fast = 0, slow = 0; fast < size; ) {
+        if(hash[str[fast]]++ == 0) {
+            str[slow] = str[fast];
+            ++slow, ++fast;
+        } else {
+            ++fast;
+        }
+    }
+    str[slow] = '\0';
+    printf("%s\n", str);
+}
+
+void rm_dup_elements_integer(int *arr, int size) {
+    int hash[1000] = {0};
+    int slow, fast;
+
+    for(slow = 0, fast = 0; fast < size; ) {
+        if(hash[arr[fast]]++ == 0) {
+            arr[slow] = arr[fast];
+            ++slow, ++fast;
+        } else {
+            ++fast;
+        }
+    }
+    for(int i = 0; i < slow; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
 int main(void) {
     int mat[][4] = {{1, 2, 3, 4},
                     {5, 6, 7, 8},
@@ -187,6 +225,13 @@ int main(void) {
                     {13, 14, 15, 16}};
 
     rotate(mat, 4);
+
+    char str[] = "abacaabn";
+    rm_dup_elements(str);   
+
+    int arr[] = {1, 7, 9, 1, 8, 1, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    rm_dup_elements_integer(arr, size); 
 
     return 0;
 }
--- test.c
+++ test.c
@@ -10,15 +10,49 @@
 
 
 //[11] remove duplicated elements
-void rm_dup_elements(int *arr, int size) {
-    
+//
+void rm_dup_elements(char *str) {
+    int hash[256] = {0};
+    int size = strlen(str);
+    int slow, fast;
+
+    for(fast = 0, slow = 0; fast < size; ) {
+        if(hash[str[fast]]++ == 0) {
+            str[slow] = str[fast];
+            ++slow, ++fast;
+        } else {
+            ++fast;
+        }
+    }
+    str[slow] = '\0';
+    printf("%s\n", str);
 }
 
+void rm_dup_elements_integer(int *arr, int size) {
+    int hash[1000] = {0};
+    int slow, fast;
 
+    for(slow = 0, fast = 0; fast < size; ) {
+        if(hash[arr[fast]]++ == 0) {
+            arr[slow] = arr[fast];
+            ++slow, ++fast;
+        } else {
+            ++fast;
+        }
+    }
+    for(int i = 0; i < slow; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
 int main(void) {
-    int arr[] = {-11, -2, 3, 4, 5};
-    sort_square(arr, 5);    
+    char str[] = "abacaabn";
+    rm_dup_elements(str);   
+
+    int arr[] = {1, 7, 9, 1, 8, 1, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    rm_dup_elements_integer(arr, size); 
 
     return 0;
 }
