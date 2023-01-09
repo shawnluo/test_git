ebc9dc9eb931c03195e035f1e7355a5519135283
Goodaytodie <andersluo@hotmail.com>
2023-01-08 22:01:00 -0800

    test

--- chapter7_dp.c
+++ chapter7_dp.c
@@ -6,9 +6,46 @@
 
 #define MAX_LEN 100
 
+//ladder climbing, one step or 2 steps once, how many ways to reach to the top
+int ladder(int n) {
+    int i, dp[n + 1];
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for(i = 3; i < n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    printf("%d\n", dp[n]);
+    return dp[n];
+}
+
 int max(int a, int b) {
     return (a > b) ? a : b;
 }
+
+static inline int min(int i, int j) {
+    return i < j ? i : j;
+}
+
+int ladder_energy(int n, int cost[n]) {
+    int i = 0;
+    int dp[n + 1];
+    dp[0] = 0;
+    dp[1] = dp[0] + cost[0];
+
+    for(i = 2; i <= n; i++) {
+        dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+    
+    int cost[5] = {6, 4, 7, 2, 1};
+    ladder_energy(5, cost);
+
+    printf("%d\n", dp[n]);
+    return 0;
+}
+
 
 //longest common subsequence
 int lcs(char *s1, char *s2, int m, int n){
--- test.c
+++ test.c
@@ -7,13 +7,28 @@
 #include <limits.h>
 #include <math.h>
 
-//ladder climbing, one step or 2 steps once, how many ways to reach to the top
-int ladder(int n) {
+
+static inline int min(int i, int j) {
+    return i < j ? i : j;
+}
+
+int ladder_energy(int n, int cost[n]) {
+    int i = 0;
+    int dp[n + 1];
+    dp[0] = 0;
+    dp[1] = 0;
+
+    for(i = 2; i <= n; i++) {
+        dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    }
+
+    printf("%d\n", dp[n]);
     return 0;
 }
 
 int main(void) {
+    int cost[5] = {6, 4, 7, 2, 1};
+    ladder_energy(5, cost);
 
-    
     return 0;
 }
