d4f6bc67714ffd679d563ae83a757f84412ce1ac
Goodaytodie <andersluo@hotmail.com>
2023-01-15 07:22:47 -0800

    update

--- test.c
+++ test.c
@@ -121,12 +121,67 @@ int move_backwards(int *arr, int size, int pos) {
 
 void merge_arrays(int *arr1, int size1, int *arr2, int size2) {
     int i = size1 - 1, j = size2 - 1;
+    int count = size1 + size2;
     while(i >= 0 || j >= 0) {
         if(i < 0) {
-            arr1[count--] = arr2[j];
+            arr1[count] = arr2[j];
+            i--, count--;
+            continue;
         }
+        if(j < 0) {
+            arr1[count] = arr1[j];
+            j--, count--;
+            continue;
+        }
+        if(arr1[i] >= arr2[j]) {
+            arr1[count] = arr1[i];
+            i--;
+        } else {
+            arr1[count] = arr2[j];
+            j--;
+        }
+        count--;
     }
 }
+
+void spiral_mat(int n) {
+    int rep = n / 2;
+    int startx = 0, starty = 0;
+    int arr[n][n];
+    int x, y;
+    int index = 0;
+    int count = 0;
+
+    while(rep--) {
+        for(y = 0; y < n - index; y++) {
+            arr[index][y] = count++;
+        }
+        for(x = 0; x < n - index; x++) {
+            arr[x][y] = count++;
+        }
+        for(; y > starty; y--) {
+            arr[x][y] = count++;
+        }
+        for(; x > startx; x--) {
+            arr[x][y] = count++;
+        }
+    }
+    if(n % 2 == 1) {
+        arr[n % 2][n % 2] = count;
+    }
+}
+
+
+void rotate(int arr[5][5]) {
+    /*
+        arr[x][y]       
+        arr[y][n - x - 1]
+        arr[n - x - 1][n - y - 1]
+        arr[n - y - 1][x]
+    */
+}
+
+
 
 int main(void) {
     //int cost[5] = {6, 4, 7, 2, 1};
