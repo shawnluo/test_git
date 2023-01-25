ad9f094073b09db57dc4ac7e98bad5453031a0b8
MarsWalker <andersluoxt@gmail.com>
2023-01-25 09:53:39 -0800

    update

---
+++ common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#endif
