74a692f17b6a10d18d6c1a8d8b22fc103d51c262
MarsWalker <andersluoxt@gmail.com>
2023-01-28 22:19:41 -0800

    update

--- test.c
+++ test.c
@@ -25,12 +25,74 @@ void ladder(int num) {
     }
 }
 
-void ladder_ext(int num) {
+void ladder_with_cost(int num) {
 
 }
 
+
+int removeDuplicates_wrong(int *nums, int numsSize) {
+    int slow = 0;
+    int fast = 0;
+
+    for(int i = 0; i < numsSize; i++) {
+        if(nums[slow] != nums[fast]) {
+            nums[++slow] = nums[fast];
+        }
+        fast++;
+    }
+    numsSize = slow + 1;
+
+    for(int i = 0; i < numsSize; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+    return numsSize;
+}
+
+int removeDuplicates(int *nums, int numsSize) {
+    int slow = 0;
+    int fast = 1;
+    int equal = 0;
+
+    while(fast < numsSize) {
+        equal = 0;
+        //slow = 0;
+        while(slow < fast) {
+            if(nums[slow] == nums[fast]) {
+                equal = 1;
+                //fast++;
+                break;
+            }
+            slow++;
+        }
+        if(equal == 0) {
+            nums[slow - 1] = nums[fast++];
+        }
+    }
+    for(int i = 0; i < slow; i++) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+}
+
+int removeDupStr(char *s) {
+    int fast, slow;
+    int hash[256] = {0};
+    for(slow = 0, fast = 0; fast < strlen(s); ) {
+        if(hash[s[fast]]++ == 0) {
+            s[slow] = s[fast];
+            ++slow, ++fast;
+        } else {
+            ++fast;
+        }
+    }
+}
+
 int main(void) {
-    fib(5);
+    int nums[] = {1, 1, 1, 8, 6, 0, 6 ,7};
+    int size = sizeof(nums) / sizeof(nums[0]);
+    int ret = removeDuplicates(nums, size);
+    printf("%d\n", ret);
 
     return 0;
 }
