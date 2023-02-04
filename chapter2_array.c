75683846e52108a5b6343c21a03cc37a79d0d986
MarsWalker <andersluoxt@gmail.com>
2023-02-03 22:25:11 -0800

    update

--- chapter2_array.c
+++ chapter2_array.c
@@ -434,6 +434,29 @@ void rotate() {
     }
 }
 
+
+void romanToInt() {
+    char s[] = "MDC";
+    int t['X' + 1] = {
+        ['I'] = 1,
+        ['V'] = 5,
+        ['X'] = 10,
+        ['L'] = 50,
+        ['C'] = 100,
+        ['D'] = 500,
+        ['M'] = 1000,
+    };
+    int res = 0;
+    for (int i = 0; s[i]; i++) {
+        if (t[s[i]] < t[s[i+1]])
+            res -= t[s[i]];
+        else
+            res += t[s[i]];
+    }
+    printf("%d\n", res);
+}
+
+
 int main(void) {
     int mat[][4] = {{1, 2, 3, 4},
                     {5, 6, 7, 8},
