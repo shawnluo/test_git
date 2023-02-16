fc26866e4e91977c83856132dc8f896b9c335a7a
MarsWalker <andersluoxt@gmail.com>
2023-02-15 22:46:30 -0800

    update

--- test.c
+++ test.c
@@ -23,51 +23,26 @@ int break_int(int n) {
 }
 
 
-/*
-    n   items
-    w   the weight which bagpack can take
-    weight[i]   ->  value[i]
 
-    dp[i][j]    max value, if pick from item[0, i], put into backpak j
-*/
-int bagpack(int weight[], int value[], int n, int w) {
-    //dp[i] the max weight which pick form item[0, i]
-    int dp[n];
-    dp[0] = 
-    for(int i = 0; i <= n; i++) {
-        dp[i] = max(dp[i - 1] + weight[i], dp[i]);
+void test() {
+    char str[80] = "This is - www.tutorialspoint.com - website";
+    const char s[2] = "-";
+    char *token;
+
+    token = strtok(str, s);
+    printf(" %s\n", token);
+    while(token != NULL) {
+        printf(" %s\n", token);
+        token = strtok(NULL, s);
     }
-
-    return dp[n];
 }
 
-
-char * addBinary(char * a, char * b){
-    int sizeA = strlen(a);
-    int sizeB = strlen(b);
-    int sizeOutput = (sizeA > sizeB ? sizeA : sizeB) + 1;
-    char * output = (char *)malloc(sizeOutput + 1);
-    int sum = 0;
-    
-    output[sizeOutput] = '\0';
-    
-    while(sizeA > 0 || sizeB > 0 || sum > 0) {
-        
-        if(sizeA > 0) {
-            sum += a[--sizeA] - '0';
-        }
-        if(sizeB > 0) {
-            sum += b[--sizeB] - '0';
-        }
-        output[--sizeOutput] = sum % 2 + '0';
-        sum /= 2;
-    }
-    return output + sizeOutput;
-}
+//TODO break_int()
 
 
 int main(void) {
-    break_int(8);
+    //break_int(8);
+    test();
 
     return 0;
 }
