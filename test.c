d08f414f58e7f21c8e2eda9e577b131cfb55f651
Goodaytodie <andersluo@hotmail.com>
2023-02-19 10:55:46 -0800

    update

--- test.c
+++ test.c
@@ -1,47 +1,30 @@
 #include "common.h"
 #include "test.h"
 
-//Given a string, return true if the s can be palindrom after deleting at most one character from it.
-int palindrom_ext(char *s) {
-    int size = strlen(s);
-    char *left = s;
-    char *right = s + size - 1;
-    int count = 0;
-    while(left < right) {
-        if(*left == *right) {
-            left++, right--;
-        } else {
-            count++;
-            left++;
+bool validWordAbbreviation(char * word, char * abbr){
+    char *pos1 = word;
+    char *pos2 = abbr;
+    //int size = strlen(word);
+
+    int num = 0;
+    while(*pos2) {
+        num = 0;
+        while(*pos2 >= '0' && *pos2 <= '9') {
+            num = num * 10 + *pos2 - '0';
+            pos2++;
         }
+        //printf("%d\n", num);
+        pos1 += num;
+        if(*pos1 != '\0' && *pos2 != '\0' && *pos1 != *pos2) {
+            return false;
+        }
+        pos1++, pos2++;
     }
-    if(count <= 1) return true;
-    count = 0;
-    while(left < right) {
-        if(*left == *right) left++, right--;
-        else    count++, right--;
-    }
-    if(count <= 1) return true;
-    return false;
+    return true;
 }
-
-double myPow(double x, int n) {
-    int N = n;
-    double res = 1.0;
-    if(n < 0) {
-        x = 1 / x;
-        N = -N;
-    }
-    for(int i = n; i != 0; i = i / 2) {
-        if(i % 2)   res = res * x;
-        x = x * x;
-    }
-    return res;
-}
-
 int main(void) {
     // break_int(8);
-    int res = myPow(3, 2);
+    int res = validWordAbbreviation("internationalization", "i12iz4n");
     printf("%d\n", res);
 
     return 0;
