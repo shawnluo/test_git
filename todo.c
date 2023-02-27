4335b775651ff607054306a780abc96e76fc48b1
Goodaytodie <andersluo@hotmail.com>
2023-02-26 17:02:24 -0800

    add todo.c

---
+++ todo.c
@@ -0,0 +1,43 @@
+#include "common.h"
+#include "test.h"
+
+
+class Solution {
+    public int calculate(String s) {
+        if (s == null || s.isEmpty()) return 0;
+        int length = s.length();
+        int currentNumber = 0, lastNumber = 0, result = 0;
+        char operation = '+';
+        for (int i = 0; i < length; i++) {
+            char currentChar = s.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                currentNumber = (currentNumber * 10) + (currentChar - '0');
+            }
+            if (!Character.isDigit(currentChar) && !Character.isWhitesp     ace(currentChar) || i == length - 1) {
+                if (operation == '+' || operation == '-') {
+                    result += lastNumber;
+                    lastNumber = (operation == '+') ? currentNumber : -currentNumber;
+                } else if (operation == '*') {
+                    lastNumber = lastNumber * currentNumber;
+                } else if (operation == '/') {
+                    lastNumber = lastNumber / currentNumber;
+                }
+                operation = currentChar;
+                currentNumber = 0;
+            }
+        }
+        result += lastNumber;
+        return result;
+    }
+}
+
+
+int main(void) {
+    // break_int(8);
+    //int res = validWordAbbreviation("internationalization", "i12iz4n");
+    //printf("%d\n", res);
+
+    quicksort();
+
+    return 0;
+}
