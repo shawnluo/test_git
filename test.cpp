f46a6e36d9dd1853c6679678bd5cc50ddc30c935
MarsWalker <andersluoxt@gmail.com>
2023-03-14 13:35:56 -0700

    update

--- test.cpp
+++ test.cpp
@@ -32,5 +32,18 @@ int main(void) {
 		cout << vec[i] << endl;
 	}
 
+	//4.
+	vec.erase(vec.begin() + 2);
+	for(unsigned int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << endl;
+	}
+
+	//5.
+	vec.erase(vec.begin() + 3, vec.begin() + 5);
+	for(vector<int>:: iterator it = vec.begin(); it != vec.end(); it++) {
+		cout << *it << endl;
+	}
+
+
 	return 0;
 }
