3da7b6683716079dca9d86a7fe5fc674e613307c
Goodaytodie <andersluo@hotmail.com>
2023-03-27 08:26:29 -0700

    update

--- test.c
+++ test.c
@@ -2,78 +2,31 @@
 #include "test.h"
 
 
-int comp(const void *x, const void *y) {
-    return *(int *)y - *(int *)x;
-}
+//TODO
+int dfs(int **mat, int x, int y, int **visited) {
 
-void test() {
-    int nums[] = {21, 8, 1, 34};
-    int len = sizeof(nums) / sizeof(nums[0]);
-    qsort(nums, len, sizeof(nums[0]), comp);
-
-    for(int i = 0; i < len; i++) {
-        printf("%d\n", nums[i]);
-    }
-}
-
-//longest increasing subarray
-int LIS(int *nums, int size) {
-    int dp[size + 1];
-    for(int i = 0; i <= size; i++) {
-        dp[i] = 1;
-    }
-    int res = 0;
-    for(int i = 1; i < size; i++) {
-        for(int j = 0; j < i; j++) {
-            if(nums[i] >= nums[j]){
-                dp[i] = fmax(dp[i], dp[j] + 1);
-            }
-        }
-        res = fmax(res, dp[i]);
-    }
-    return res;
 }
 
 
-/*  [123] - merge intervals
-    [1, 8] [2, 9]   -> [1, 9]
-    [1, 8] [9, 17]  -> [1, 8] [9, 17]
-    1. sortint: 
-        1). by first element
-        2). if first elements are equal. then by send elements
-    2. travel from second paire. L and R point to first and second
-    3. the core concept: how to get L and R?
-        1). L and R are set to the first pair array
-        2). the conditions of updating L and R?
-*/
-int merge_intervals(int **nums, int row, int col) {
-    int **res = (int **)malloc(row * sizeof(int *));
-    for(int i = 0; i < col; i++) {
-        res[i] = (int *)malloc(col * sizeof(int));
-    }
-
-    int L = nums[0][0];
-    int R = nums[0][1];
-    int idx = 0;
-
-    for(int i = 1; i < row; i++) {
-        if(nums[i][0] <= R && nums[i][1] >= R) {
-            R = nums[i][1];
-        } else if(nums[i][0] > R) {
-            res[idx][0] = L;
-            res[idx][1] = R;
-            L = res[idx][0];
-            R = res[idx][1];
-            idx++;
-        }
-    }
-    res[idx][0] = L;
-    res[idx][1] = R;
-    return res;
+int dfs_matrix(int **mat, int x, int y, int **visited) {
+    if(x < 0 || x >= m || y < 0 || y >= n || mat[x][y] == 1 )
+    dfs(mat, x - 1, y, visited);
 }
 
 
 int main(void) {
-    test();
+    int **mat = (int **)malloc(3 * sizeof(int *));
+    for(int i = 0; i < 3; i++) {
+        mat[i] = (int *)malloc(5 * sizeof(int));
+    }
+
+    int **visited = (int **)malloc(3 * sizeof(int *));
+    for(int i = 0; i < 3; i++) {
+        visited[i] = (int *)malloc(5 * sizeof(int));
+    }
+
+    dfs_matrix(mat, 3, 5, visited);
+    //printf("%s\n", s);
+
     return 0;
 }
