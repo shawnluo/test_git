258679d8956abfa230e582c35d48e701d826f259
goodaytodie <andersluoxt@gmail.com>
2023-08-22 02:28:34 -0700

    prog

--- test2.cpp
+++ test2.cpp
@@ -2,26 +2,31 @@
 
 // using namespace std;
 
-int main() {
-    // vector<string> s = {"abc", "123"};
-    // string su;
-    // su.push_back(s[0][0]);
-    // su.push_back(s[1][0]);
-
-    // cout << su << endl;
- 
-    string s = "abc";
-
-    //1. 
-    string s1(s); 
-    reverse(s1.begin(), s1.end());
-
-    //2. 
-    string s2;
-    for(auto x : s) {
-        s2.insert(0, 1, x);
+int do_search(vector<int> nums, int start, int end, int target) {
+    if(start > end) {
+        return -1;
     }
-    cout << s2 << endl;
+    int mid = (start + end) / 2;
+    if(nums[mid] == target) {
+        return mid;
+    }
+    if(target < nums[mid]) {
+        return do_search(nums, start, mid - 1, target);
+    } else {
+        return do_search(nums, mid + 1, end, target);
+    }
+}
+
+//TODO fix this 
+int searchRe(vector<int> nums, int target) {
+    return do_search(nums, 0, nums.size() - 1, target);
+
+}
+int main(void) {
+    vector<int> nums{1, 3, 5, 9};
+    int target = 19;
+    int ret = searchRe(nums, target);
+    cout << ret << endl;
 
     return 0;
 }
