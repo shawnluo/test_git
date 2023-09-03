8093afb10b38c2b1bb1bc41d03a2dce3ce8703a4
goodaytodie <andersluoxt@gmail.com>
2023-09-02 23:46:13 -0700

    u

--- julie_backtracking.cpp
+++ julie_backtracking.cpp
@@ -2,7 +2,46 @@
 #include <string.h>
 
 
-int comibination_include_k_numbers(int *arr,int n, int k)
+int combination_include_k_numbers(int *pArr,int n, int k)
 {
-    
+    if((pArr == NULL)
+    ||(n < 0)
+    ||(k < 0))
+    {
+        return -1;
+    }
+
+    int n = 0;
+    int k = 2;
+    int i = 0;
+    int base_idx = 0;
+    int current_idx = 0;
+    int j = 0;
+    int  gap = 0;
+    int rest_len = 0;
+
+    for(i = 0;i < n;i++)
+    {
+        base_idx = i;
+        gap = 0;
+        while(1)
+        {
+            if(((base_idx+gap+1) < n-1) && ((base_idx+gap+k-1) < n-1))
+            {
+                //print out the first element
+                printf("\n %d",pArr[base_idx]);
+                
+                //print out the second element
+                printf("\n %d",pArr[base_idx])+gap+1);
+                
+                current_idx = pArr[base_idx]+gap+2;
+                //print out the remaining elements
+                for(rest_len = k-2; rest_len>=0;rest_len--)
+                {
+                    
+                }
+                printf("\n  ")
+            }
+    }
+
 }
