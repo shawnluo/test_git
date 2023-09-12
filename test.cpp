3955bf77510881050df5edb0471a9a2769e43451
goodaytodie <andersluoxt@gmail.com>
2023-09-12 00:16:08 -0700

    u

--- test.cpp
+++ test.cpp
@@ -239,7 +239,23 @@ pNode reverseLL(pNode pHead) {
 
 // malloc aligned memory
 void *memAlign(size_t size, size_t alignment) {
-    // 1. 
+    // 1. the new length
+    int offset = alignment - 1;
+    int newSize = size + offset + sizeof(size_t);
+    // 2. malloc
+    size_t *addr = (size_t *)malloc(newSize);
+    // 3. the newAddr
+    void *newAddr = (void *)((size_t)(addr + offset) & ~(offset));
+
+    // 4. save the diffencial
+    *(size_t *)(newAddr - 1) = (size_t *)newAddr - addr;
+
+    return newAddr;
+}
+
+void align_free(void * aligned_addr) {
+    void *addr = (size_t *)aligned_addr - *((size_t *)aligned_addr - 1);
+    free(addr);
 }
 
 // binary tree
