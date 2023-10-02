5a5dc8f008aeaeba31a5e3b85ca795e8e3452b69
MarsWalker <andersluoxt@gmail.com>
2023-10-01 23:42:06 -0700

    u

--- test.cpp
+++ test.cpp
@@ -1,326 +1,59 @@
 #include "test.hpp"
 
-
-// TODO: dp
-
-// 0.                            - 01背包
-    // 1. 二维数组
-    // 2. 一维滚动数组 - 也就是把二维数组数据压缩到只有一行。重复使用这一行数据(就是carl所说的拷贝)：
-            // 1. 从上次的数据取值，将本次计算后的值，覆盖掉本行。因为二维数组的行列矩阵数据只被斜下部的单元使用。
-            // 2. j需要倒序。因为根据dp[j] = dp[j - weight[i]] + value，dp[j]是一维的，其取值是根据左边的数据来获取的。
-                // 如果从左边开始循环，那么左边的取值会被不断重复计入。从而影响结果。
-                // 如果从右边开始循环，因为左边的元素都是初始值0。其不会影响计算结果！
-                // 对比二维数组，其数据都是独立存在的，没有被重复使用。
-
-// 1. 装满背包的最大价值是多少？    - 纯完全背包
-// 2. 装满背包有多少种方法？求组合  - 零钱兑换2
-// 3. 装满背包有多少种方法？求排列  - 组合总和4
-// 4. 装满背包最少用多少个物品？    - 322零钱兑换
-
-// 5. 最长连续上升子序列
-    // 1）. 长度
-    // 2）. 打印出上升子序列
-
-// 6. 最长上升子序列
-    // 1）. 长度
-    // 2）. 打印出上升子序列
-
-// 7. 排序后的单元的序列长度
-    // 1). 双指针去重
-    // 2). 判断与前一个数不相等
-    // 3). 除重后的长度 - dp解
-    // 4). 打印除重后的序列 - unordered_set
-
-// 8. 未排序后的单元的序列长度
-    // 1）. 除重后的长度
-        // 1. dp
-        // 2. while loop delete
-    // 2）. 打印除重后的序列
-
-// 9. leetcode 1091 - the shortest path in binary matrix
-    // 1. BFS - breadth-first search
-
-// 24 完全平方数
-// dp[j] = min(dp[j], dp[j - i * i] + 1)
-
-// void test() {
-//     vector<int> nums{-1, 1, 1, 2, 2, 2, 3, 3};
-
-//     int low = 0;
-//     for(int i = 0; i < nums.size() - 1; i++) {
-//         if(nums[i] != nums[i + 1]) {
-//             nums[low++] = nums[i];
-//         }
-//     }
-//     nums[low] = nums.back();
-//     cout << nums.back() << endl;
-
-//     for(int i = 0; i <= low; i++) {
-//         cout << nums[i] << " ";
-//     }
-//     cout << endl;
-// }
-
-// int lenOfNorep(vector<int>& nums) {
-//     int count = 1;
-
-//     for(int i = 1; i < nums.size(); i++) {
-//         if(nums[i] != nums[i - 1]) {
-//             nums[count] = nums[i];
-//             count++;
-//         }
-//     }
-//     return count;
-// }
-
-// void permutation(string s, int pos) {
-//     int size = s.size();
-//     if(pos == size){
-//         cout << s << endl;
-//     }
-//     for(int i = pos; i < size; i++) {
-//         swap(s[i], s[pos]);
-//         permutation(s, pos + 1);
-//         swap(s[i], s[pos]);
-//     }
-// }
-
-// void removeExtraSpaces(string& s) {
-//     int size = s.size();
-//     int fast = 0;   // fast travels along old string
-//     int slow = 0;   // slow points to new string
-
-//     for(int fast = 0; fast < size; fast++) {
-//         if(s[fast] != ' ') {
-//             if(slow != 0) {
-//                 s[slow++] = ' ';
-//             }
-//             while(fast < size && s[fast] != ' ') {
-//                 s[slow++] = s[fast++];
-//             }
-//         }
-//     }
-//     s.resize(slow);
-// }
-
-/*
-    1: 
-    2: a, b, c
-    3: d, e, f
-    4: g, h, i
-    5: j, k, l
-    6: m, n, o
-    7: p, q, r, s
-    8: t, u, v
-    9: w, x, y, z
-
-    "23": abc, def -> ad, ae, af, ...
-*/
-// const string letterMap[10] = {...};
-// void bt_5(const string& digits, int index) {
-//     if(index == digits.size()) {
-//         res.push_back(s);
-//         return;
-//     }
-//     for(int i = 0; i < letters.size(); i++) {
-//         s.push_back(letters[i]);
-//         bt_5(digits, index + 1);
-//         s.pop_back();
-//     }
-// }
-
-
-void getNext(vector<int>& next, string s) {
+int removeVal(vector<int>& nums, int val) {
     int left = 0;
-    next[0] = 0;
-    int size = s.size();
+    int right = 0;
 
-    for(int right = 1; right < size; right++) {
-        while(left > 0 && s[right] != s[left]) {
-            left = s[left - 1];
+    for(int right = 0; right < nums.size(); right++) {
+        if(nums[right] != val) {
+            nums[left++] = nums[right];
         }
-        if(s[right] == s[left]) {
+    }
+    nums.resize(left);
+    return left;
+}
+
+int removeVal_ext(vector<int>& nums, int val) {
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while(left < right) {
+        while(left < right && nums[left] != val) {
             left++;
         }
-        s[right] = left;
-    }
-}
-
-int myStrstr(string s, string t) {
-    int j = 0;  // prefix
-    vector<int> next(t.size(), 0);
-    getNext(next, t);
-    for(int i = 0; i < s.size(); i++) { // i: surfix
-        while(j > 0 && s[i] != t[j]) {
-            j = next[j - 1];
+        while(left < right && nums[right] == val) {
+            right--;
         }
-        if(s[i] == t[j]) {
-            j++;
-        }
-        if(j == t.size()) {
-            return i - t.size() + 1;
+        if(left < right) {
+            nums[left++] = nums[right--];
         }
     }
-    return - 1;
+    return left;
 }
 
-void matRotate(vector<vector<int>>& mat) {
-    int size = mat.size();
-    for(int i = 0; i < size / 2; i++) {
-        for(int j = i; j < size - i - 1; j++) {
-            int save = mat[i][j];
-            mat[i][j] = mat[j][size - i - 1];
-            mat[j][size - i - 1] = mat[size - i - 1][size - j - 1];
-            mat[size - i - 1][size - j - 1] = mat[size - j - 1][i];
-            mat[size - j - 1][i] = save;
+vector<int> twoSum_BF(vector<int> nums, int target) {
+    // unordered_set<int> set;
+    vector<int> res;
+    for(int i = 0; i < nums.size(); i++) {
+        for(int j = i + 1; j < nums.size(); j++) {
+            if(nums[i] + nums[j] == target) {
+                res.push_back(nums[i]);
+                res.push_back(nums[j]);
+                return res;
+            }
         }
     }
+    return {};
 }
-
-int partition(int* nums, int start, int end) {
-    int j = start - 1;
-
-    for(int i = start; i < end; i++) {
-        if(nums[i] > nums[end]) {
-            j++;
-            swap(nums[i], nums[j]);
-        }
-    }
-    swap(nums[j], nums[end]);
-    return j;
-}
-
-void quickSort() {
-
-}
-
-
-//TODO nth biggest number
-
 
 int main(void) {
-    // string s = "  abc  d  e ";
-    // removeExtraSpaces(s);
-    // cout << s << endl;
-    // cout << s.size() << endl;
+    vector<int> nums{1, 32, 6, 8, 0, 4, 6, 43};
+    cout << removeVal(nums, 6) << endl;
 
-    // string s = "show me";
-    // string t = "mpe";
-
-    // cout << myStrstr(s, t) << endl;
-
-    int nums[] = {2, 8, 9, 12, 22, 3, 10};
-
+    for(auto x : nums) {
+        cout << x << " ";
+    }
+    cout << endl;
 
     return 0;
 }
-
-void getNext(int *next, string s) {
-    next[0] = 0;
-    int j = 0;
-
-    for(int i = 1; i < s.size(); i++) {
-        while(j > 0 && s[i] != s[j]) {
-            j = next[j - 1];
-        }
-        if(s[i] == s[j]) {
-            j++;
-        }
-        s[i] = j;
-    }
-
-}
-
-int myStrstr(string s, string t) {
-    int next[t.size()];
-    getNext(next, t);
-
-    int j = 0;
-    for(int i = 0; i < s.size(); i++) {
-        while(j > 0 && s[i] != t[j]) {
-            j = next[j - 1];
-        }
-        if(s[i] == t[j]) {
-            j++;
-        }
-        if(j == t.size()) {
-            return i - t.size() + 1;
-        }
-    }
-    return -1;
-}
-
-void mat(vector<vector<int>>& mat) {
-    int n = mat.size();
-    for(int i = 0; i < n / 2; i++) {
-        for(int j = i; j < n - i - 1; j++) {
-            int save = mat[i][j];
-            mat[i][j] = mat[j][n - i - 1];
-            mat[j][n - i - 1] = mat[n - i - 1][n - j - 1];
-            mat[n - i - 1][n - j - 1] = mat[n - j - 1][i];
-            mat[n - j - 1][i] = save;
-        }
-    }
-}
-
-void spiral(int n) {
-    int half = n / 2;
-
-    int startX = 0;
-    int startY = 0;
-    int offset = 1;
-    vector<vector<int>> mat(n, vector<int>(n));
-    while(half-- > 0) {
-        int x = startX;
-        int y = startY;
-
-        for(; y > n - offset; y++) {
-            mat[x][y] = count++;
-        }
-        for(; x > n - offset; x++) {
-            mat[x][y] = count++;
-        }
-        for(; y > startY; y--) {
-            mat[x][y] = count++;
-        }
-        for(; x > startX; x--) {
-            mat[x][y] = count++;
-        }
-    }
-    if(n % 2) mat[x][y] = count;
-}
-
-int partition(int* arr, int start, int end) {
-    int j = start - 1;
-
-    for(int i = start; i < end; i++) {
-        if(arr[i] < arr[end]) {
-            swap(arr[++j], arr[i]);
-        }
-    }
-    swap(arr[j], arr[end]);
-    return j;
-}
-
-void myQsort(int *arr, int start, int end) {
-    if(start < end) {
-        int pivot = partition(arr, start, end);
-        myQsort(arr, start, pivot - 1);
-        myQsort(arr, pivot + 1, end);
-    }
-}
-
-int do_getNth(int* arr, int start, int end, int k) {
-    if(start < end) {
-        int pivot = partition(arr, start, end);
-        if(k == pivot) return arr[pivot];
-        
-        if(k < pivot)   return do_getNth(arr, start, pivot - 1);
-        else            return do_getNth(arr, pivot + 1, end);
-    }
-}
-
-int getNth(int *arr, int len, int k) {
-    return do_getNth(arr, 0, len - 1, k);
-}
-
--- test1.cpp
+++ test1.cpp
@@ -1,214 +1,48 @@
 
 #include "test.hpp"
 
-void delExtraSpaces(string& s) {
-    int j = 0;
-    for(int i = 0; i < s.size(); i++) {
-        if(s[i] != ' ') {
-            if(j != 0) {
-                s[j++] = ' ';
-            }
-            while(i < s.size() && s[i] != ' ') {
-                s[j++] = s[i++];
-            }
+unordered_set<int> interSection(vector<int>& nums1, vector<int>& nums2) {
+    unordered_set<int> res;
+    unordered_set<int> set(nums1.begin(), nums1.end());
+
+    for(int num : nums2) {
+        if(set.find(num) != set.end()) {
+            res.insert(num);
         }
     }
-    s.resize(j);
-
-    // cout << "START" << s << "END" << endl;
-}
-
-void reverse(string& s, int start, int end) {
-    if(s.size() <= 1) {
-        return;
-    }
-
-    int left = start;
-    int right = end;
-
-    while(left < right) {
-        swap(s[left++], s[right--]);
-    }
-}
-
-/*
-    show me the money -> money the me show
-*/
-void reverseWords(string& s) {
-    delExtraSpaces(s);
-    reverse(s, 0, s.size() - 1);
-
-    int start = 0;
-    int end = 0;
-    for(int i = 0; i < s.size(); i++) {
-        if(s[i] == ' ') {
-            end = i - 1;
-            reverse(s, start, end);
-            start = i + 1;
-        }
-    }
-    reverse(s, start, s.size() - 1);
-}
-
-int calLen(vector<int>& nums) {
-    int count = 1;
-
-    for(int i = 1; i < nums.size(); i++) {
-        if(nums[i] != nums[i - 1]) {
-            nums[count] = nums[i];
-            count++;
-        }
-    }
-    nums.resize(count);
-    return count;
-}
-
-uint32_t getOnes(uint32_t num) {
-    int count = 0;
-
-    while(num) {
-        num &= (num - 1);
-        count++;
-    }
-    return count;
-}
-
-/*  1. 注意const的使用
-    2. 注意类型转换
-
-    memory copy: copy all the contents for len
-    string copy: copy will stop when meet a '\0'
-*/
-void* memCpy(void* dest, const void* src, size_t count) {
-    char* pDest = static_cast<char*>(dest);     // 强制类型转换 - 编译时期
-    const char* pSrc = static_cast<const char*>(src);
-
-    for(size_t i = 0; i < count; ++i) {
-        pDest[i] = pSrc[i];
-    }
-    return dest;
-}
-
-int meetingRoom(vector<vector<int>> nums) {
-    int room = 0;
-    vector<int> start;
-    vector<int> end;
-    for(auto x : nums) {
-        start.push_back(x[0]);
-        end.push_back(x[1]);
-    }
-    sort(start.begin(), start.end());
-    sort(end.begin(), end.end());
-
-    int size = nums.size();
-    int pEnd = 0;
-    for(int pStart = 0; pStart < size; pStart++) {
-        if(pStart == 0 || start[pStart] < end[pEnd]) {
-            room++;
-        } else {
-            pEnd++;
-        }
-    }
-    return room;
-}
-
-int longest_u(string s) {
-    int hash[256];
-    int len = 0;
-    int pos = -1;
-    int res = 0;
-    for(int i = 0; i < 256; i++) {
-        hash[i] = -1;
-    }
-
-    for(int i = 0; i < 256; i++) {
-        // cout << hash[i] << " ";
-    }
-    
-    for(int i = 0; i < s.size(); i++) {
-        pos = max(pos, hash[s[i]]);
-        len = i - pos;
-        res = max(res, len);
-        hash[s[i]] = i;
-    }
+    // return vector<int>(res.begin(), res.end());
     return res;
 }
 
-int myRead(char* buf, int n) {
-    int readChar = 4;
-    char buf4[4];
-    int count = 0;
-    
-    while(readChar == 4 && cout < n) {
-        readChar = read4(buf4);
-        for(int i = 0; i < readChar; i++) {
-            if(count == n) {
-                return count;
-            }
-            buf[count++] = buf4[i];
+vector<int> towSum(vector<int>& nums, int target) {
+    unordered_map <int, int> map;
+
+    for(int i = 0; i < nums.size(); i++) {
+        auto iter = map.find(target - nums[i]);
+        if(iter != map.end()) {
+            return {iter->second, i};
         }
+        map.insert(pair<int, int>(nums[i], i));
     }
-
-    return count;
-}
-
-bool isRect(vector<vector<int>> nums) {
-    // 6 lines len
-    // sort
-    // if long * long == short * short + shorter * shorter; return true;
-    // else return false;
-}
-
-void permu(string s, int pos) {
-    if(pos == s.size()) {
-        cout << s << endl;
-        return;
-    }
-    for(int i = pos; i < s.size(); i++) {
-        swap(s[i], s[pos]);
-        permu(s, pos + 1);
-        swap(s[i], s[pos]);
-    }
-}
-
-void bt(int n, int k, int pos) {
-    if(buf.size() == k) {
-        res.push_back(buf);
-        return;
-    }
-    for(int i = 1; i <= n; i++) {
-        buf.push_back(i);
-        bt(n, k, i + 1);
-        buf.pop();
-    }
+    return {};
 }
 
 int main(void) {
-    string s = "abcada";
-    cout << longest_u(s) << endl;
-    // vector<vector<int>> nums = {{1, 3}, {2, 6}, {7, 9}};
-    // cout << meetingRoom(nums) << endl;
-    // uint32_t n = 4;
-    // cout << getOnes(n) << endl;
+    // vector<int> nums1 = {1, 2, 2, 1};
+    // vector<int> nums2 = {2, 2};
 
-    // vector<int> nums = {2, 1, 3, 16, 51, 14};
-    // test_revers_iterator(nums);
+    // unordered_set<int> res = interSection(nums1, nums2);
 
-    // string s = "  show   me the    money  ";
-    // reverseWords(s);
-    // cout << s << endl;
-    
-    // vector<int> nums{1, 1, 1, 2, 3, 4, 4, 5};
-    // cout << calLen(nums) << endl;
-
-    // for(auto x : nums) {
+    // for(auto x : res) {
     //     cout << x << " ";
     // }
     // cout << endl;
 
-    //     for(vector<int>::iterator it = nums.begin(); it != nums.end(); it++) {
-    //     cout << *it << endl;
-    // }
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int>res = towSum(nums, target);
+
+    cout << res[0] << " " << res[1] << endl;
 
     return 0;
 }
