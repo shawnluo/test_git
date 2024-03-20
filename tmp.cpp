d2edea07505b52339731132ceac8a7ad6c029445
Goodaytodie <andersluo@hotmail.com>
2024-03-20 02:15:08 -0700

    u

--- tmp.cpp
+++ tmp.cpp
@@ -1,30 +1,70 @@
 
 #include "test.hpp"
 
-
-int dp_12(vector<int>& weight, vector<int>& value, int BAG) {
-    vector<int> dp(weight.size() + 1, 0);
-    for(int i = 0; i < weight.size(); i++) {
-        for(int j = BAG; j >= weight[i]; j--) {
-            dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
+int getSum(vector<vector<int>>& tmp, int x, int y, int size) {
+    x = x + size / 2;
+    y = y + size / 2;
+    int res = 0;
+    for(int i = -size / 2; i < size / 2; i++) {
+        for(int j = -size / 2; j < size / 2; j++) {
+            res += tmp[i + x][j + y];
         }
     }
-    return dp[BAG];
+    return res;
 }
 
-int Add(int num1, int num2) {
-    int sum, carry = 1;
-    while(carry) {
-        sum = num1 ^ num2;
-        carry = (num1 & num2) << 1;
-        num1 = sum;
-        num2 = carry;
+void spiral(vector<vector<int>>& mat, vector<vector<int>>& newMat, int size) {
+    int n = mat.size();
+    int half = n / 2;
+    int startX = 0;
+    int startY = 0;
+    int offset = 1;
+
+    int newN = n + size / 2 * 2;    // 新的padding后的矩阵长度。如果size是3，则要加上 3 / 2 * 2 = 2。如果是5，则要加上 5 / 2 * 2 = 4
+    newMat.resize(n, vector<int>(n));
+
+    // copy mat to newN
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            newMat[i + size / 2][j + size / 2] = mat[i][j];
+        }
     }
-    return sum;
+
+    vector<vector<int>> tmp(newN, vector<int>(newN, 0));
+    // int count = 1;
+    half = 1;
+    while(half--) {
+        int x = startX;
+        int y = startY;
+        for(; y < newN - offset; y++) tmp[x][y] = 0;
+        for(; x < newN - offset; x++) tmp[x][y] = 0;
+        for(; y > startY; y--) tmp[x][y] = 0;
+        for(; x > startX; x--) tmp[x][y] = 0;
+
+        startX++;
+        startY++;
+        offset++;
+    }
+
+    // blur
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            newMat[i][j] = getSum(tmp, i , j, size);
+        }
+    }
 }
+
 
 int main(void) {
-    cout << Add(1, -5) << endl;
+    vector<vector<int>> mat = {{1, 2, 3, 4, 5},
+                                {6, 7, 8, 9, 10},
+                                {11, 12, 13, 14, 15},
+                                {16, 17, 18, 19, 20},
+                                {21, 22, 23, 24, 25}};
+    vector<vector<int>> newMat; 
+    spiral(mat, newMat, 5);
+
+    showMeV(newMat);
 
     return 0;
 }
